7e4c5b4b113f2a59e61b0cd4bd37cb97e2e41580
FranquitoDotado <sebasrodri578@gmail.com>
2023-10-09 17:57:50 -0300

    avance 1

---
+++ README2.md
@@ -0,0 +1,11 @@
+Flujo:
+- <!-- Llamar al json --> 
+- Recorrer los productos
+- Armar template de card
+- Armar imágenes
+- Armar cícurlos de color
+- Armar texto inferior
+- Título y precio
+Las siguientes funciones deben estar disponibles luego de que se rendericen los productos
+- Dar funcionalidad de carrousel a las imágenes
+- Dar funcionadidad de cambio de imágen cuando hago click en circulo de color
--- script.js
+++ script.js
@@ -1,56 +1,85 @@
-async function loadProducts() {
+async function getProducts() {
     try {
-        const response = await fetch("./data/products.json");
-        if (!response.ok) {
-            throw new Error('No se pudo cargar el archivo JSON');
-        }
-
-        const data = await response.json();
-        const productList = document.getElementById('product-list');
-
-        for (let i = 0; i < 20; i++) {
-            productList.appendChild(template(data.products[i]));
-        }
-
+        let response = await fetch("./data/products.json")
+        let products = await response.json()
+        return products
     } catch (error) {
-        console.error('Error al cargar los productos:', error);
+        return error
     }
 }
 
-function template(product) {
-    let template = document.createElement("div")
-    template.append(product.title)
-    images(product.images, template)
-    return template
-}
-
-function images(images, template) {
-    let res = ""
-    for (let i = 0; i < images.length; i++) {
-        const element = images[i];
-        const image = document.createElement("img")
-        image.src = element.src
-        template.append(image)
+async function mapProducts() {
+    try {
+        let response = await getProducts()
+        let productSection = document.querySelector("#test-product-list")
+        response.forEach(productData => {
+            let productTemplate = template(productData)
+            productSection.appendChild(productTemplate)
+        });
+    } catch (error) {
+        return error
     }
-    return res
 }
 
-function carousel(images, template) {
-let elem = document.querySelector('.carousel');
-products.forEach(producto => {
-    const imgElement = document.createElement('img');
-    imgElement.src = producto.imagen;
-    carousel.appendChild(imgElement);
-});
-let flkty = new Flickity(element, {
-    // options
-    cellAlign: 'left',
-    contain: true
-});
+async function template(productData) {
+    try {
+        let productDiv = document.createElement("div");
+        productDiv.classList.add("each-product");
+        productDiv.innerHTML = `
+        <div class="top-div-product">
+            <div class="menu">
+                <div class="container-menu">
+                    <div class="menu-items">
+                        <p>S</p>
+                        <p>M</p>
+                        <p>L</p>
+                        <p>XL</p>
+                        <p>XXL</p>
+                    </div>
+                </div>
+            </div>
+            <div class="main-carousel">
+                <div class="carousel-cell"><img src="data/asdasdas.png"></div>
+                <div class="carousel-cell"><img src="data/asdasdas.png"></div>
+            </div>
+        </div>
+        <div class="bottom-div-product">
+            <div>
+                <p>${productData.title}</p>
+                <div class="circulo">
+                </div>
+            </div>
+            <div>
+                <p>$300</p>
+            </div>
+        </div>`
+        return productDiv
+    } catch (error) {
+        return error
+    }
 }
 
-    
+function images() {
 
+}
 
+function colors() {
 
-loadProducts();
+}
+
+function info() {
+
+}
+
+function flickityCarousel() {
+    let elem = document.querySelector('.main-carousel');
+    let flkty = new Flickity(elem, {
+        cellAlign: 'left',
+        contain: true
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('La página se ha cargado completamente.');
+    mapProducts();
+});
